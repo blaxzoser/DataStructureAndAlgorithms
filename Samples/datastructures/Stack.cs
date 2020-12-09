c487b4d27bbc379f4b9cb7bef31a0c69e032f20e
Luis Alberto Pintado Goicochea <luiscol50@hotmail.com>
2020-12-08 21:24:16 -0600

    Added Stack Case

---
+++ Samples/datastructures/Stack.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Samples.datastructures
+{
+    /// <summary>
+    /// LIFO = Last in First Out
+    /// </summary>
+    public class Stack
+    {
+
+        private Node _head;
+        private int _size;
+        public class Node 
+        {
+            int _data;
+            public Node Next { get; set; }
+            public Node(int data)
+            {
+                this._data = data;
+            }
+
+            public int Data { get { return _data; } }
+        }
+
+        /// <summary>
+        /// O(1)
+        /// </summary>
+        /// <param name="data"></param>
+        public void Push(int data) 
+        {
+            //Create new node
+            //Set it's next to be head
+            //Set head to be the new one
+            var newNode = new Node(data);
+            newNode.Next = _head;            
+            _head = newNode;
+            _size++;
+        }
+
+        /// <summary>
+        /// O(1)
+        /// </summary>
+        public int Pop() 
+        {
+            //Store the value you want to return
+            //Set the current head.next to be the new head
+            //return the value
+            if (_head == null) throw new Exception("its empty the head");
+
+            int data = _head.Data;
+            _head = _head.Next;
+            _size--;
+            return data;
+        }
+
+        public int Peek() { return _head.Data; }
+
+        public bool IsEmpty() { return _head == null; }
+
+        public int Size => _size;
+     
+    }
+}
---
+++ Test/StackTest.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using Samples.datastructures;
+
+namespace Test
+{
+    public class StackTest
+    {
+        private Stack _stack;
+
+        [SetUp]
+        public void Setup()
+        {
+            _stack = new Stack();
+        }
+
+        [Test]
+        public void Push()
+        {
+            _stack.Push(1);
+            _stack.Push(2);
+            _stack.Push(3);
+
+            Assert.AreEqual(3, _stack.Size);
+        }
+
+        [Test]
+        public void Pop()
+        {
+            _stack.Push(1);
+            _stack.Push(2);
+            _stack.Push(3);
+
+            Assert.AreEqual(3, _stack.Pop());
+            Assert.AreEqual(2, _stack.Size);
+        }
+
+        [Test]
+        public void Peek()
+        {
+            _stack.Push(1);
+            _stack.Push(2);
+            _stack.Push(3);
+
+            Assert.AreEqual(3, _stack.Peek());
+            Assert.AreEqual(3, _stack.Size);
+
+            Assert.AreEqual(3, _stack.Pop());
+            Assert.AreEqual(2, _stack.Peek());
+        }
+    }
+}

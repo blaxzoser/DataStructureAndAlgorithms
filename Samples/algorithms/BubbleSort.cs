f075d2a7b37d19858f3d45027f78b86ebeede020
Luis Alberto Pintado Goicochea <luiscol50@hotmail.com>
2020-12-20 21:16:40 -0600

    Added new BubbleSort algorimth

---
+++ Samples/algorithms/BubbleSort.cs
@@ -0,0 +1,47 @@
+﻿
+namespace Samples.algorithms
+{
+    public class BubbleSort
+    {
+        /// <summary>
+        /// O(n^2)
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public int[] Sort(int[] array) 
+        {
+            for (int i = array.Length; i > 0; i--)
+            {
+                for (int y = 0; y < i-1 ; y++)
+                {
+                    if(array[y] > array[y+1])
+                    {
+                        var temp = array[y];
+                        array[y] = array[y + 1];
+                        array[y + 1] = temp;
+                    }
+                }
+            }
+
+            return array;
+        }
+
+        public int[] SortRefactor(int[] array)
+        {
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                for (int y = 0; y < array.Length-i-1; y++)
+                {
+                    if (array[y] > array[y + 1])
+                    {
+                        var temp = array[y];
+                        array[y] = array[y + 1];
+                        array[y + 1] = temp;
+                    }
+                }
+            }
+
+            return array;
+        }
+    }
+}
---
+++ Test/algorithms/BubbleSortTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using Samples.algorithms;
+
+namespace Test.algorithms
+{
+    public class BubbleSortTest
+    {
+        private BubbleSort _bubblesort;
+        [SetUp]
+        public void SetUp() 
+        {
+            _bubblesort = new BubbleSort();
+        }
+
+        [Test]
+        public void Sort() 
+        {
+            int[] array = { 5, 1, 4, 2, 8 };
+            int[] sorted = _bubblesort.Sort(array);
+
+            Assert.AreEqual(1, sorted[0]);
+            Assert.AreEqual(2, sorted[1]);
+            Assert.AreEqual(4, sorted[2]);
+            Assert.AreEqual(5, sorted[3]);
+            Assert.AreEqual(8, sorted[4]);
+        }
+
+        [Test]
+        public void SortRefactor()
+        {
+            int[] array = { 5, 1, 4, 2, 8 };
+            int[] sorted = _bubblesort.SortRefactor(array);
+
+            Assert.AreEqual(1, sorted[0]);
+            Assert.AreEqual(2, sorted[1]);
+            Assert.AreEqual(4, sorted[2]);
+            Assert.AreEqual(5, sorted[3]);
+            Assert.AreEqual(8, sorted[4]);
+        }
+    }
+}

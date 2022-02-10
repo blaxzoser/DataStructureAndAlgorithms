8138f412346078e4cb2c8ea5784af6edd10582eb
Luis Alberto Pintado Goicochea <39611932+blaxzoser@users.noreply.github.com>
2022-02-10 12:43:55 -0600

    New algorithm

---
+++ Samples/questions/lamdas/Parangram.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Samples.questions.arraysAndStrings
+{
+
+    // https://www.codewars.com/kata/545cedaa9943f7fe7b000048/csharp
+    //    A pangram is a sentence that contains every single letter of the alphabet at least once.For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once(case is irrelevant).
+    //   Given a string, detect whether or not it is a pangram.Return True if it is, False if not.Ignore numbers and punctuation.
+    public class Parangram
+    {
+        public static bool IsParangramLamdas(string text)
+        {
+            return text
+                .ToLower()
+                .Where(character => Char.IsLetter(character))
+                .Distinct()
+                .Count() == 26;
+        }
+
+        public static bool IsPangram(string str)
+        {
+            char[] letters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (!str.ToLower().Contains(letters[i]))
+                    return false;
+            }
+            return true;
+        }
+
+    }
+}
---
+++ Test/OtherTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Samples.questions.arraysAndStrings;
+
+namespace Test
+{
+
+    public class OtherTest
+    {
+
+        [Test]
+        public void TestParangram()
+        {
+            Assert.AreEqual(true, Parangram.IsPangram("The quick brown fox jumps over the lazy dog."));
+        }
+
+        [Test]
+        public void TestParangramWithLambdas()
+        {
+            Assert.AreEqual(true, Parangram.IsParangramLamdas("The quick brown fox jumps over the lazy dog."));
+        }
+    }
+}

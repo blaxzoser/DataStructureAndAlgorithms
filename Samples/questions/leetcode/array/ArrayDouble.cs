ba6ab2f1563b65954a5209263b6a6217ca413929
lpintado <lpintado@webmd.net>
2025-04-19 17:15:07 -0600

    Add ArrayDouble class for rotating 2D matrices
    Introduces the `ArrayDouble` class with a `RotateImage` method that rotates a 2D matrix by 90 degrees clockwise in-place. Also adds `ArrayDoubleTest` for unit testing using NUnit, including setup and a test case to validate the rotation functionality.

---
+++ Samples/questions/leetcode/array/ArrayDouble.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Samples.questions.leetcode.array
+{
+    public class ArrayDouble
+    {
+        /// <summary>
+        /// You are given an n x n 2D matrix representing an image, 
+        /// rotate the image by 90 degrees (clockwise).
+        // You have to rotate the image in-place,
+        // which means you have to modify the input 2D matrix directly.
+        // DO NOT allocate another 2D matrix and do the rotation.
+        // e.g 1
+        // Input: [[1,2,3],[4,5,6],[7,8,9]]:
+        // { 1, 2, 3},
+        // { 4, 5, 6},
+        // { 7, 8, 9 }
+        // Output : [[7,4,1],[8,5,2],[9,6,3]]
+        // { 7, 4, 1},
+        // { 8, 5, 2},
+        // { 9, 6, 3 }
+
+        // e.g 2
+        // Input: [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+        // { 5, 1, 9, 11},
+        // { 2, 4, 8, 10},
+        // { 13, 3, 6, 7 }
+        // { 15, 14, 12, 16 }
+        // Output : [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+        // { 15, 13, 2, 5},
+        // { 14, 3, 4,  1},
+        // { 12, 6, 8 , 9}
+        // { 16, 7, 10 ,11}
+
+
+        /// </summary>
+        /// <param name="matrix"></param>
+        public void RotateImage(int[][] matrix)
+        {
+            int n = matrix.Length;
+            // Step 1: Transpose the matrix
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    int temp = matrix[i][j];
+                    
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
+            }
+
+            // Step 2: Reverse each row
+            for (int i = 0; i < n; i++)
+            {
+                Array.Reverse(matrix[i]);
+            }
+        }
+    }
+}
---
+++ Test/leetcode/ArrayDoubleTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using Samples.questions.leetcode.array;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test.leetcode
+{
+    public class ArrayDoubleTest
+    {
+        ArrayDouble arrayDouble;
+
+        [SetUp]
+        public void Setup()
+        {
+            arrayDouble = new ArrayDouble();
+        }
+
+        [Test]
+        public void ShouldReturnTotalSum()
+        {
+            int[][] input = new int[3][]
+            {
+                new int[] {1, 2, 3},
+                new int[] {4, 5, 6},
+                new int[] {7, 8, 9}
+            };
+
+            int[][] output = new int[3][]
+           {
+                new int[] {7, 4, 1},
+                new int[] {8, 5, 2},
+                new int[] {9, 6, 3}
+           };
+
+            arrayDouble.RotateImage(input);
+
+            Assert.AreEqual(input, output);
+        }
+
+    }
+}
